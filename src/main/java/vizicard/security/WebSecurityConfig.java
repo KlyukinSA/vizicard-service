75222d91c038c2856cdba4d86319aae594356883
daniil1234567 <89739241+daniil1234567@users.noreply.github.com>
2023-08-07 13:38:11 +0300

    cors

--- src/main/java/vizicard/security/WebSecurityConfig.java
+++ src/main/java/vizicard/security/WebSecurityConfig.java
@@ -67,15 +67,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers("/h2-console/**/**");;
   }
 
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-      }
-    };
-  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {

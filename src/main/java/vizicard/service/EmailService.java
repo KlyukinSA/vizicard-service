f4216e67d2401c8eee139bd4274938b2d111f4e2
KlyukinSA <ctepa.kl@mail.ru>
2023-08-07 20:07:18 +0300

    start EMAIL

---
+++ src/main/java/vizicard/service/EmailService.java
@@ -0,0 +1,25 @@
+package vizicard.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+
+    public void send(String to) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("info@vizicard.ru");
+        message.setTo(to);
+        message.setSubject("meet");
+        message.setText("person");
+
+        System.out.println(message);
+        emailSender.send(message);
+        System.out.println(message);
+    }
+}

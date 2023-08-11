c799231909d62ade50b28cf8f766de933f340235
KlyukinSA <ctepa.kl@mail.ru>
2023-08-11 09:34:10 +0300

    fix updateAvatar() and Background

---
+++ src/tests/test_avatar.sh
@@ -0,0 +1,4 @@
+jwt=`bash signup.sh $1`
+bash see_profile.sh $jwt
+bash avatar.sh $jwt
+bash see_profile.sh $jwt
--- src/tests/upload_avatar.sh
+++
@@ -1,2 +0,0 @@
-curl -X POST "http://localhost:8080/users/me/avatar" -H  "accept: */*" -H  "Content-Type: multipart/form-data" -F "file=@login.sh;type=text/plain" -H 'Authorization: Bearer '$1
-

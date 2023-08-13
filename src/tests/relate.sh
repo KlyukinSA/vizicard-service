0a9b3d13b2b78429dc6ba869812f2cfd27e99d15
KlyukinSA <ctepa.kl@mail.ru>
2023-08-13 13:41:08 +0300

    Relation: get vcard also if not first time, filter bad targetProfileId correctly

--- src/tests/relate.sh
+++ src/tests/relate.sh
@@ -1,1 +1,1 @@
-curl -X POST "http://localhost:8081/users/3/relations" -H  "accept: */*" -H 'Authorization: Bearer '$1
+curl -X POST "http://localhost:8081/users/11/relations" -H  "accept: */*" -H 'Authorization: Bearer '$1

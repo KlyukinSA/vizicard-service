d3dc50de22592e88c067a02ab8ba038d5c04ba6e
KlyukinSA <ctepa.kl@mail.ru>
2023-08-21 18:41:27 +0300

    uploadFile POST /files

--- src/main/java/vizicard/controller/S3Controller.java
+++ src/main/java/vizicard/controller/S3Controller.java
@@ -2,8 +2,14 @@ package vizicard.controller;
 
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import vizicard.dto.ProfileResponseDTO;
+import vizicard.model.CloudFile;
 import vizicard.service.S3Service;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/files")
@@ -12,11 +18,11 @@ public class S3Controller {
 
     private final S3Service service;
 
-//    @PostMapping("upload")
-////    @PreAuthorize("isAuthenticated()")
-//    public ResponseEntity<?> uploadFile(@RequestPart("file") MultipartFile file) throws IOException {
-//        return ResponseEntity.ok().body(service.uploadFile(file));
-//    }
+    @PostMapping
+    @PreAuthorize("isAuthenticated()")
+    public CloudFile uploadFile(@RequestPart("file") MultipartFile file) throws IOException {
+        return service.uploadFile(file);
+    }
 
     @GetMapping("{id}")
     public String getUrlById(@RequestParam("id") Integer id) {

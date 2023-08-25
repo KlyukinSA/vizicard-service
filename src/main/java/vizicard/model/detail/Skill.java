01997b6ff67f22e812e9f3f16f5740476bcb4cdb
KlyukinSA <ctepa.kl@mail.ru>
2023-08-25 11:59:22 +0300

    UniqueConstraint(columnNames = {"owner_id", "skill"}

--- src/main/java/vizicard/model/detail/Skill.java
+++ src/main/java/vizicard/model/detail/Skill.java
@@ -6,10 +6,13 @@ import vizicard.model.Profile;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.util.Date;
 @Entity
 @Data
 @NoArgsConstructor
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"owner_id", "skill"}))
 public class Skill extends DetailBase {
 
     private String skill;

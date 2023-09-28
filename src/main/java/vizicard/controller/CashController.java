dd80da75f098db89e2358f116c6be79988550f60
KlyukinSA <ctepa.kl@mail.ru>
2023-09-28 21:31:32 +0300

    referral: add cash bonus for referral creator

--- src/main/java/vizicard/controller/CashController.java
+++ src/main/java/vizicard/controller/CashController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vizicard.model.Profile;
+import vizicard.model.Relation;
+import vizicard.model.RelationType;
 import vizicard.repository.ProfileRepository;
+import vizicard.repository.RelationRepository;
 import vizicard.utils.ProfileProvider;
 
 @RestController
@@ -13,11 +16,19 @@ import vizicard.utils.ProfileProvider;
 @RequiredArgsConstructor
 public class CashController {
     private final ProfileRepository profileRepository;
+    private final RelationRepository relationRepository;
     private final ProfileProvider profileProvider;
     @PostMapping
     public void addCash(float amount) {
         Profile user = profileProvider.getUserFromAuth();
         user.setCash(user.getCash() + amount);
         profileRepository.save(user);
+
+        Relation created = relationRepository.findByTypeAndProfile(RelationType.CREATED_REFERRAL, user);
+        if (created != null) {
+            Profile creator = created.getOwner();
+            creator.setCash((float) (creator.getCash() + 0.05 * amount));
+            profileRepository.save(creator);
+        }
     }
 }

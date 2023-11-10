281ace6d9575a957883f5af0637420be69b3785a
KlyukinSA <ctepa.kl@mail.ru>
2023-11-10 20:41:19 +0300

    Contact: forbid to create full contact without description

--- src/main/java/vizicard/service/ContactService.java
+++ src/main/java/vizicard/service/ContactService.java
@@ -29,6 +29,9 @@ public class ContactService {
         contact.setOwner(profileProvider.getUserFromAuth().getCurrentCard());
         contactRepository.save(contact);
         contact.setOrder(contact.getId());
+        if (contact.isFull() && contact.getDescription() == null) {
+            throw new CustomException("cant create full contact without description", HttpStatus.BAD_REQUEST);
+        }
         return contactRepository.save(contact);
     }
 
